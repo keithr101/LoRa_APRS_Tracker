b4be6b87bc857cc82a8331c6902f403859de5635
keithr101 <64606656+keithr101@users.noreply.github.com>
2025-04-12 16:12:53 -0400

    Changed to accomodate a configurable 192,168.X subnet

--- src/wifi_utils.cpp
+++ src/wifi_utils.cpp
@@ -10,18 +10,27 @@ extern      logging::Logger     logger;
 
 uint32_t    noClientsTime        = 0;
 
+#ifndef AP_ADDRESS_IP
+    #define AP_ADDRESS_IP   "192.168.4.1"
+#endif
 
 namespace WIFI_Utils {
 
     void startAutoAP() {
+        IPAddress ap_ip((uint32_t)0);
+        ap_ip.fromString(AP_ADDRESS_IP);
+        IPAddress ap_gateway((uint32_t)ap_ip);
+        //NOTE: for other than 192.168.x.y addresses this needs additional logic
+        IPAddress ap_subnet((uint32_t)0x00FFFFFF); //255.255.255.0
         WiFi.mode(WIFI_MODE_NULL);
         WiFi.mode(WIFI_AP);
+        WiFi.softAPConfig(ap_ip, ap_gateway, ap_subnet);
         WiFi.softAP("LoRaTracker-AP", Config.wifiAP.password);
     }
 
     void checkIfWiFiAP() {
         if (Config.wifiAP.active || Config.beacons[0].callsign == "NOCALL-7"){
-            displayShow(" LoRa APRS", "    ** WEB-CONF **","", "WiFiAP:LoRaTracker-AP", "IP    :   192.168.4.1","");
+            displayShow(" LoRa APRS", "    ** WEB-CONF **","", "WiFiAP:LoRaTracker-AP", "IP   :  " AP_ADDRESS_IP ,"");
             logger.log(logging::LoggerLevel::LOGGER_LEVEL_WARN, "Main", "WebConfiguration Started!");
             startAutoAP();
             WEB_Utils::setup();
